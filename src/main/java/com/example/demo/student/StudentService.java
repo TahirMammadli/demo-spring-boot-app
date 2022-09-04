3877e02e0760388a28aa02bd56725abf76690281
Tahir Mammadli <TahirMammadli13@gmail.com>
2022-09-04 10:50:47 -0400

    First commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,21 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        24,
+                        1L,
+                        "Tahir",
+                        LocalDate.of(1998, Month.JULY, 13),
+                        "tahirmammadli13@gmail.com"
+                )
+        );
+    }
+}
