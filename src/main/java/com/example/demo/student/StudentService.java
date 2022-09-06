c70b521c8df68a60538338cd8281c5058cd58ba8
Tahir Mammadli <TahirMammadli13@gmail.com>
2022-09-06 11:51:06 -0400

    added db connection

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,13 @@
 package com.example.demo.student;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Component
 public class StudentService {
     public List<Student> getStudents() {
         return List.of(
